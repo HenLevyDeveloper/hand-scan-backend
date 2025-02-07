bce476c663320288b442a682868a8aa2e759c76d
HenLevyDeveloper <www.hen@gmail.com>
2025-02-07 20:11:08 +0200

    Update server.js

--- server.js
+++ server.js
@@ -18,4 +18,4 @@ app.use('/api/scan', scanRoutes);
 app.use('/api/products', productsRoutes);
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`✅ Server running on port ${PORT}`));
