c4cbf13e12d9abdfef950280d54f336c82f02c0b
HenLevyDeveloper <www.hen@gmail.com>
2025-02-07 22:47:16 +0200

    scannable body parts

--- routes/scan.js
+++ routes/scan.js
@@ -36,4 +36,56 @@ router.post('/upload', authMiddleware, upload.single('scan'), async (req, res) =
     }
 });
 
+
+// Body Parts
+router.get('/body-parts', (req, res) => {
+    res.json([
+    {
+        "id": 1,
+        "name": "Wrist",
+        "image": "https://upload.wikimedia.org/wikipedia/commons/b/b2/Nadgarstek_%28ubt%29.jpeg"
+    },
+    {
+        "id": 2,
+        "name": "Thumb",
+        "image": "https://www.publicdomainpictures.net/pictures/180000/velka/hand-with-thumb-up.jpg"
+    },
+    {
+        "id": 3,
+        "name": "Forearm",
+        "image": "https://paincareclinic.co.uk/wp-content/uploads/2023/08/forearm.png"
+    },
+    {
+        "id": 4,
+        "name": "Elbow",
+        "image": "https://excelsportspt.com/wp-content/uploads/2020/03/shutterstock_1523898098.jpg"
+    },
+    {
+        "id": 5,
+        "name": "Hand",
+        "image": "https://i.pinimg.com/736x/16/3b/a5/163ba5058014b2b8aada92fc10947301.jpg"
+    },
+    {
+        "id": 6,
+        "name": "Ankle",
+        "image": "https://upload.wikimedia.org/wikipedia/commons/7/74/Ankle.jpg"
+    },
+    {
+        "id": 7,
+        "name": "Foot",
+        "image": "https://www.thechelseaclinic.uk/wp-content/uploads/2022/08/foot-drop-729x1024.jpg"
+    },
+    {
+        "id": 8,
+        "name": "Lower Leg",
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQy112VzyzeN4Tf_-8znz_uX6VzKEM8v6o58w&s"
+    },
+    {
+        "id": 9,
+        "name": "Knee",
+        "image": "https://www.londonbridgeorthopaedics.co.uk/wp-content/uploads/2024/06/knee.jpg"
+    }
+]);
+});
+
 module.exports = { router };
